d310c772061ee79226dbc48924865b6008b36698
Josiah Hickey <98180394+TrickyHickey@users.noreply.github.com>
2022-04-07 15:01:09 -0400

    Update Makingdatabase.py

--- Makingdatabase.py
+++ Makingdatabase.py
@@ -1,3 +1,12 @@
 def main():
+
+    team_info = {
+        'name': 'Maple Leafs', 
+        'city': 'Toronto'
+
+    }
+
+    full_name = "The " + team_info['city'] + team_info['name']
+
     pass
 main()
